1836055a0f20253f2dba7d5c6cd5da4b124d432c
Rizwan Tejpar <rtejpar@gmail.com>
2013-12-09 17:19:58 -0500

    added missing endpoint url tests

--- spec/storify_spec.rb
+++ spec/storify_spec.rb
@@ -17,4 +17,20 @@ describe Storify do
     Storify::versioned_api(:secure => false, 
                            :version => 2).should eq 'http://api.storify.com/v2'
   end
+
+  it "should retrieve the auth API endpoint" do
+    Storify::auth.should eq 'https://api.storify.com/v1/auth'
+  end
+
+  it "should retrieve the stories API endpoint" do
+    Storify::stories.should eq 'https://api.storify.com/v1/stories'
+  end
+
+  it "should retrieve the user-specific stories API endpoint" do
+    Storify::userstories(@username).should eq "https://api.storify.com/v1/stories/#{@username}"
+  end
+
+  it "should retrieve a user-specific story API endpoint" do
+    Storify::story(@username, "mystory").should eq "https://api.storify.com/v1/stories/#{@username}/mystory"
+  end
 end
